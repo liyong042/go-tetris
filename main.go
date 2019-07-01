46a8929418b8cb352a55d3ed802173bddcdbfc4b
liyong042 <40205311+liyong042@users.noreply.github.com>
2019-07-01 20:27:29 +0800

    Update main.go
    测试一下 键盘事件

--- main.go
+++ main.go
@@ -1,38 +1,27 @@
-
 package main
+
 import (
 	"fmt"
-	"math/rand"
-	"time"
 
 	"github.com/nsf/termbox-go"
 )
 
-func draw() {
+func draw(ch rune) {
 	w, h := termbox.Size()
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-	for y := 0; y < h; y++ {
-		for x := 0; x < w; x++ {
-			termbox.SetCell(x, y, ' ', termbox.ColorDefault, termbox.Attribute(rand.Int()%250)+1)
-		}
-	}
+	termbox.SetCell(w/2, h/2, ch, termbox.ColorRed, termbox.ColorDefault)
+
 	termbox.Flush()
 }
 
 func main() {
-	fmt.Println("start")
-
-	err := termbox.Init()
-	if err != nil {
-		panic(err)
-	}
+	termbox.Init()
 	defer termbox.Close()
 
 	for {
-		draw()
-		time.Sleep(2000 * time.Millisecond)
+		//ev := termbox.PollEvent()
+		//draw(ev.Ch)
 	}
-
-	fmt.Println("stop")
+	fmt.Println("Stop")
 
 }
