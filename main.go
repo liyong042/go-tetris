1d0ed295d8dbd865f7748adb7ee7ffdb43014f0b
liyong042 <40205311+liyong042@users.noreply.github.com>
2019-07-01 20:32:09 +0800

    Update main.go
    增加消行功能

--- main.go
+++ main.go
@@ -15,33 +15,35 @@ import (
 //游戏地图
 const (
 	backColor   = termbox.ColorBlue
-	brickSize   = 4 //方块数量
-	brickWidth  = 12  //游戏方块宽
-	brickHeight = 18  //游戏方块高
-	backGround = `
-		WWWWWWWWWWWW  WWWWWW
-		WkkkkkkkkkkW  WkkkkW
-		WkkkkkkkkkkW  WkkkkW
-		WkkkkkkkkkkW  WkkkkW
-		WkkkkkkkkkkW  WkkkkW
-		WkkkkkkkkkkW  WWWWWW
-		WkkkkkkkkkkW
-		WkkkkkkkkkkW
-		WkkkkkkkkkkW  BBBBBB
-		WkkkkkkkkkkW  WWWWWW
-		WkkkkkkkkkkW
-		WkkkkkkkkkkW
-		WkkkkkkkkkkW  BBBBBB
-		WkkkkkkkkkkW  WWWWWW
-		WkkkkkkkkkkW
-		WkkkkkkkkkkW  BBBBBB
-		WkkkkkkkkkkW  WWWWWW
-		WkkkkkkkkkkW
-		WkkkkkkkkkkW
-		WWWWWWWWWWWW
-
-		kkkkkkkkkkkkkkkkkkkk
-		WWWWWWWWWWWWWWWWWWWW
+	brickSize   = 4  //方块数量
+	brickWidth  = 12 //游戏方块宽
+	brickHeight = 21 //游戏方块高
+	panelX      = 3  //游戏面板偏移
+	panelY      = 1  //游戏面板偏移
+	backGround  = `
+		WWWWWWWWWWWWWW  WWWWWWWW
+		WkkkkkkkkkkkkW  WkkkkkkW
+		WkkkkkkkkkkkkW  WkkkkkkW
+		WkkkkkkkkkkkkW  WkkkkkkW
+		WkkkkkkkkkkkkW  WkkkkkkW
+		WkkkkkkkkkkkkW  WkkkkkkW
+		WkkkkkkkkkkkkW  WkkkkkkW
+		WkkkkkkkkkkkkW  WWWWWWWW
+		WkkkkkkkkkkkkW  
+		WkkkkkkkkkkkkW
+		WkkkkkkkkkkkkW
+		WkkkkkkkkkkkkW  BBBBBBBB
+		WkkkkkkkkkkkkW  WWWWWWWW  
+		WkkkkkkkkkkkkW  
+		WkkkkkkkkkkkkW
+		WkkkkkkkkkkkkW
+		WkkkkkkkkkkkkW  BBBBBBBB
+		WkkkkkkkkkkkkW  WWWWWWWW
+		WkkkkkkkkkkkkW
+		WkkkkkkkkkkkkW
+		WkkkkkkkkkkkkW
+		WkkkkkkkkkkkkW  BBBBBBBB
+		WWWWWWWWWWWWWW  WWWWWWWW
 	`
 )
 
@@ -56,26 +58,15 @@ var (
 	nextBrick     Brick //下一个方块
 	curBrickIndex = 0   //当前方块方向
 	curBrickType  = 0   //当前方块种类
-	curBkColor    = termbox.ColorRed
-	nextBkColor   = termbox.ColorRed
+	curBkColor    = 0   //当前背景颜色
+	nextBkColor   = 0   //下一个背景颜色
 	nextBrickType = 0   //下一个方块种类
-
 	//地图颜色
-	colorMapping  = map[rune]termbox.Attribute{
+	colorMapping = map[rune]termbox.Attribute{
 		'k': termbox.ColorBlack,
 		'K': termbox.ColorBlack | termbox.AttrBold,
-		'r': termbox.ColorRed,
-		'R': termbox.ColorRed | termbox.AttrBold,
-		'g': termbox.ColorGreen,
-		'G': termbox.ColorGreen | termbox.AttrBold,
-		'y': termbox.ColorYellow,
-		'Y': termbox.ColorYellow | termbox.AttrBold,
 		'b': termbox.ColorBlue,
 		'B': termbox.ColorBlue | termbox.AttrBold,
-		'm': termbox.ColorMagenta,
-		'M': termbox.ColorMagenta | termbox.AttrBold,
-		'c': termbox.ColorCyan,
-		'C': termbox.ColorCyan | termbox.AttrBold,
 		'w': termbox.ColorWhite,
 		'W': termbox.ColorWhite | termbox.AttrBold,
 	}
@@ -84,8 +75,7 @@ var (
 	// -1  0  0 -1
 	// -1  0  0 -1
 	// -1 -1 -1 -1
-	brickArray = [brickHeight+2 ][brickWidth+2 ]int{}
-
+	brickArray = [brickHeight + 2][brickWidth + 2]int{}
 	//方块种类
 	// 0000 0
 	// 0000 0
@@ -100,7 +90,19 @@ var (
 		{322, 71, 113, 47}, //L型方块
 		{1111, 9, 1111, 9}, //-型方块 可以考虑使用16进制
 	}
+	//方块随机颜色
+	brickColors = []termbox.Attribute{
+		termbox.ColorBlack,
+		termbox.ColorRed,
+		termbox.ColorGreen,
+		termbox.ColorYellow,
+		termbox.ColorBlue,
+		termbox.ColorMagenta,
+		termbox.ColorCyan,
+		termbox.ColorWhite,
+	}
 )
+
 //地图字符对应颜色
 func getColorByCh(ch rune) termbox.Attribute {
 	if c, ok := colorMapping[ch]; ok {
@@ -108,6 +110,7 @@ func getColorByCh(ch rune) termbox.Attribute {
 	}
 	return backColor
 }
+
 //画游戏地图
 func drawBackGround(text string, left, top int) {
 	lines := strings.Split(text, "\n")
@@ -117,23 +120,26 @@ func drawBackGround(text string, left, top int) {
 		}
 	}
 }
+
 //画背景
 func drawBack(x, y int, color termbox.Attribute) {
 	termbox.SetCell(2*x-1, y, ' ', backColor, color)
 	termbox.SetCell(2*x, y, ' ', backColor, color)
 }
+
 //画方块格子
 func drawBlock(x, y int, color termbox.Attribute) {
-
 	termbox.SetCell(2*x-1, y, ' ', backColor, color)
 	termbox.SetCell(2*x, y, ' ', backColor, color)
 }
+
 //画一种方块
-func drawBrick(x, y int, brick *Brick, color termbox.Attribute ) {
+func drawBrick(x, y int, brick *Brick, c int) {
 	for i := 0; i < brickSize; i++ {
-		drawBlock(x+brick[i].x, y+brick[i].y, color )
+		drawBlock(x+brick[i].x, y+brick[i].y, brickColors[c])
 	}
 }
+
 //根据方块图创建一个方块
 func createBrick(t int) (bk Brick) {
 	cnt := 0
@@ -154,18 +160,31 @@ func createBrick(t int) (bk Brick) {
 	}
 	return bk
 }
+
 //画图
 func draw() {
 	termbox.Clear(backColor, backColor)
-	drawBackGround(backGround, 1, 0)    //画游戏地图
+	drawBackGround(backGround, 1, 0) //画游戏地图
 	drawBrickArray()
-	drawBrick(curPosX, curPosY, &curBrick,curBkColor ) 		//画当前方块
-	drawBrick( 17, 2, &nextBrick , nextBkColor  ) 	    //画下一个方块
- 	termbox.Flush()
+	drawBrick(curPosX+panelX, curPosY+panelY, &curBrick, curBkColor) //画当前方块
+	drawBrick(20, 2, &nextBrick, nextBkColor)                        //画下一个方块
+	termbox.Flush()
 }
+
+//画方块的数组矩阵
+func drawBrickArray() {
+	for i := 0; i < brickHeight+2; i++ {
+		for j := 0; j < brickWidth+2; j++ {
+			if t := brickArray[i][j]; t > 0 {
+				drawBlock(panelX+j, panelY+i, brickColors[t])
+			}
+		}
+	}
+}
+
 //产生一个随机方块
 func createRandBrick() {
-	curPosX = 6
+	curPosX = 4
 	curPosY = 0
 	curBrickType = nextBrickType
 	curBrickIndex = 0
@@ -173,13 +192,14 @@ func createRandBrick() {
 	curBrick = nextBrick
 	curBkColor = nextBkColor
 
-	nextBrickType = rand.Intn(len(brickMap))
+	nextBrickType = rand.Intn( len(brickMap) )
 	nextBrick = createBrick(brickMap[nextBrickType][0])
-	nextBkColor = termbox.Attribute(rand.Intn( 7 ) )
+	nextBkColor = rand.Intn( len(brickColors) -1  ) + 1
 }
+
 //是否可以放得下
-func isPut(  x, y int , bk *Brick ) bool {
-	for i:=0; i< brickSize ;i++ {
+func isPut(x, y int, bk *Brick) bool {
+	for i := 0; i < brickSize; i++ {
 		tY := bk[i].y + y
 		tX := bk[i].x + x
 		if brickArray[tY][tX] != 0 {
@@ -189,57 +209,74 @@ func isPut(  x, y int , bk *Brick ) bool {
 	return true
 }
 
-//画方块的数组矩阵
-func  drawBrickArray(){
-	for i:= 0;i< brickHeight+2; i++{
-		for j:=0;j< brickWidth+2;j++ {
-			if t:=brickArray[i][j] ; t >0 {
-				drawBlock(  j , i , termbox.Attribute(t) )
-			}
-		}
+//检查是否可以删行
+func checkFull() {
+	for y := brickHeight; y >= 1; y-- {
+		removeFull(y)
 	}
 }
+
+//删除满的行
+func removeFull(y int) {
+	for x := 1; x <= brickWidth; x++ {
+		if brickArray[y][x] == 0 {
+			return
+		}
+	}
+	for y = y - 1; y >= 1; y-- {
+		for x := 1; x <= brickWidth; x++ {
+			brickArray[y+1][x] = brickArray[y][x]
+		}
+
+	}
+}
+
 //加入方块数组
-func addBrickToMap( x, y int , bk *Brick ,  cl termbox.Attribute ){
-	for i:=0;i<	brickSize;i++{
+func addBrickToMap(x, y int, bk *Brick, c int) {
+	for i := 0; i < brickSize; i++ {
 		tY := bk[i].y + y
 		tX := bk[i].x + x
-		brickArray[tY][tX] =  int( cl )
+		brickArray[tY][tX] = c
 	}
 }
 
 //向下移动
 func moveDown() {
-	if isPut( curPosX, curPosY+1, &curBrick ) {
+	if isPut(curPosX, curPosY+1, &curBrick) {
 		curPosY += 1
-	}else {
-		addBrickToMap( curPosX, curPosY+1, &curBrick, curBkColor  )
+	} else {
+		addBrickToMap(curPosX, curPosY, &curBrick, curBkColor)
 		createRandBrick()
 	}
 }
 
 //向左
 func moveLeft(x int) {
-	if isPut( curPosX+x , curPosY, &curBrick ) {
+	if isPut(curPosX+x, curPosY, &curBrick) {
 		curPosX += x
 	}
 }
 
 //向上
 func moveUp() {
-	curBrickIndex++
-	if curBrickIndex >= brickSize {
-		curBrickIndex = 0
+
+	t := curBrickIndex +1
+	if t >= brickSize {
+		t = 0
 	}
-	curBrick = createBrick(brickMap[curBrickType][curBrickIndex])
+	bk := createBrick(brickMap[curBrickType][t])
+	if isPut(curPosX, curPosY, &bk ) {
+		curBrickIndex = t
+		curBrick = bk
+	}
 }
 
-func initGame(){
+func initGame() {
 	rand.Seed(time.Now().Unix())
-	for i:= 0;i< brickHeight+2; i++{
-		for j:=0;j< brickWidth+2;j++{
+	for i := 0; i < brickHeight+2; i++ {
+		for j := 0; j < brickWidth+2; j++ {
 			brickArray[i][j] = 0
-			if i==0 || i==brickHeight+1 || j==0 || j== brickWidth+1 {
+			if i == 0 || i == brickHeight+1 || j == 0 || j == brickWidth+1 {
 				brickArray[i][j] = -1
 			}
 		}
@@ -287,6 +324,7 @@ func main() {
 			}
 		case <-ticker.C:
 			moveDown()
+			checkFull()
 		}
 	}
 }
