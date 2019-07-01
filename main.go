f77257d7b98b1753ed138a1a614b8ea5f7d624f7
liyong042 <40205311+liyong042@users.noreply.github.com>
2019-07-01 20:29:17 +0800

    Update main.go
    画游戏界面

--- main.go
+++ main.go
@@ -1,38 +1,94 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
-
 	"github.com/nsf/termbox-go"
+	"strings"
 )
+//常量声明
+//游戏地图
+const (
+	backColor  = termbox.ColorBlue
+	backGround = `
+		WWWWWWWWWWWW  WWWWWW
+		WkkkkkkkkkkW  WkkkkW
+		WkkkkkkkkkkW  WkkkkW
+		WkkkkkkkkkkW  WkkkkW
+		WkkkkkkkkkkW  WkkkkW
+		WkkkkkkkkkkW  WWWWWW
+		WkkkkkkkkkkW
+		WkkkkkkkkkkW
+		WkkkkkkkkkkW  BBBBBB
+		WkkkkkkkkkkW  WWWWWW
+		WkkkkkkkkkkW
+		WkkkkkkkkkkW
+		WkkkkkkkkkkW  BBBBBB
+		WkkkkkkkkkkW  WWWWWW
+		WkkkkkkkkkkW
+		WkkkkkkkkkkW  BBBBBB
+		WkkkkkkkkkkW  WWWWWW
+		WkkkkkkkkkkW
+		WkkkkkkkkkkW
+		WWWWWWWWWWWW
 
-func draw() {
-	w, h := termbox.Size()
-	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-	for y := 0; y < h; y++ {
-		for x := 0; x < w; x++ {
-			termbox.SetCell(x, y, ' ', termbox.ColorDefault, termbox.Attribute(rand.Int()%250)+1)
+		kkkkkkkkkkkkkkkkkkkk
+		WWWWWWWWWWWWWWWWWWWW
+	`
+)
+//地图颜色
+var (
+	colorMapping = map[rune]termbox.Attribute{
+		'k': termbox.ColorBlack,
+		'K': termbox.ColorBlack | termbox.AttrBold,
+		'r': termbox.ColorRed,
+		'R': termbox.ColorRed | termbox.AttrBold,
+		'g': termbox.ColorGreen,
+		'G': termbox.ColorGreen | termbox.AttrBold,
+		'y': termbox.ColorYellow,
+		'Y': termbox.ColorYellow | termbox.AttrBold,
+		'b': termbox.ColorBlue,
+		'B': termbox.ColorBlue | termbox.AttrBold,
+		'm': termbox.ColorMagenta,
+		'M': termbox.ColorMagenta | termbox.AttrBold,
+		'c': termbox.ColorCyan,
+		'C': termbox.ColorCyan | termbox.AttrBold,
+		'w': termbox.ColorWhite,
+		'W': termbox.ColorWhite | termbox.AttrBold,
+	}
+)
+//地图字符对应颜色
+func getColorByCh(ch rune) termbox.Attribute {
+	if c, ok := colorMapping[ch]; ok {
+		return c
+	}
+	return backColor
+}
+//画游戏地图
+func drawBackGround(text string, left, top int) {
+	lines := strings.Split(text, "\n")
+
+	for y, line := range lines {
+		for x, ch := range line {
+			drawBlock(left+x, top+y, getColorByCh(ch))
 		}
 	}
+}
+//画方块格子
+func drawBlock(x, y int, color termbox.Attribute) {
+	termbox.SetCell(2*x-1, y, ' ', backColor, color)
+	termbox.SetCell(2*x, y, ' ', backColor, color)
+}
+//画图
+func draw() {
+	termbox.Clear(backColor, backColor)
+	drawBackGround(backGround, 1, 0)  //画游戏地图
 	termbox.Flush()
 }
 
 func main() {
-	fmt.Println("start")
-
-	err := termbox.Init()
-	if err != nil {
-		panic(err)
-	}
+	termbox.Init()
 	defer termbox.Close()
-
 	for {
 		draw()
-		time.Sleep(2000 * time.Millisecond)
+		termbox.PollEvent()
 	}
-
-	fmt.Println("stop")
-
 }
