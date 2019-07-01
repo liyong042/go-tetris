b89ccfedd204cb5e03ff15f7e5f841612cb28962
liyong042 <40205311+liyong042@users.noreply.github.com>
2019-07-01 20:26:58 +0800

    Update main.go
    简单画一下界面

--- main.go
+++ main.go
@@ -1,1 +1,38 @@
 
+package main
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/nsf/termbox-go"
+)
+
+func draw() {
+	w, h := termbox.Size()
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			termbox.SetCell(x, y, ' ', termbox.ColorDefault, termbox.Attribute(rand.Int()%250)+1)
+		}
+	}
+	termbox.Flush()
+}
+
+func main() {
+	fmt.Println("start")
+
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	for {
+		draw()
+		time.Sleep(2000 * time.Millisecond)
+	}
+
+	fmt.Println("stop")
+
+}
